83bf9a4c027e0488ca72d740239507e79eadf411
wasselessalah <163673177+wasselessalah@users.noreply.github.com>
2024-08-23 12:59:39 +0100

    ccc

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -71,13 +71,7 @@ export async function POST(req: Request) {
 
     const newUser = await createUser(user);
 
-    if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id,
-        },
-      });
-    }
+
 
     return NextResponse.json({ message: "OK", user: newUser });
   }
